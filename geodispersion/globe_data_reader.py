8eceab7761931c1481b45b75b00777fab3d365f0
FrancescoPinto02 <francesco.pintop@gmail.com>
2025-07-02 13:48:19 +0200

    Fix: fix file to __file__

--- geodispersion/globe_data_reader.py
+++ geodispersion/globe_data_reader.py
@@ -6,7 +6,7 @@ import pandas as pd
 def read_data(globe_data_path: str | Path = None):
     if globe_data_path is None:
         # Ottieni la directory dello script corrente
-        script_dir = Path(file).parent
+        script_dir = Path(__file__).parent
         globe_data_path = script_dir / "GLOBE-Phase-2-Aggregated-Societal-Culture-Data.xls"
     else:
         globe_data_path = Path(globe_data_path)
